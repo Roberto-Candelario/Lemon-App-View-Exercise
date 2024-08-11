7bb8900711d1357b9d2b379c6e587fb7a36fa1b7
Roberto Candelario <r-jc@live.com>
2024-08-11 17:05:39 -0400

    Initial Commit

---
+++ Lemon App View Exercise/Lemon_App_View_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lemon_App_View_ExerciseApp.swift
+//  Lemon App View Exercise
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Lemon_App_View_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lemon App View ExerciseTests/Lemon_App_View_ExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lemon_App_View_ExerciseTests.swift
+//  Lemon App View ExerciseTests
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import Testing
+@testable import Lemon_App_View_Exercise
+
+struct Lemon_App_View_ExerciseTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
